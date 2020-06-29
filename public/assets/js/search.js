6d3fd9c21c5b5afe1b4e602675be209a7a4a46cb
oualich <ouali.cherikh@gmail.com>
2020-06-29 16:44:20 +0200

    update

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -185,55 +185,56 @@ async function searchGIFs() {
         const arrayMusic = [];
 
         $('#amplitude-player').append(`
+        <!-- Left Side Player -->
         <div id="amplitude-left">
             <img data-amplitude-song-info="cover_art_url"/>
             <div id="player-left-bottom">
-            <div id="time-container">
-            <span class="current-time">
-                <span class="amplitude-current-minutes" ></span>:<span class="amplitude-current-seconds"></span>
-            </span>
-            <div id="progress-container">
-                <input type="range" class="amplitude-song-slider"/>
-                <progress id="song-played-progress" class="amplitude-song-played-progress"></progress>
-                <progress id="song-buffered-progress" class="amplitude-buffered-progress" value="0"></progress>
-            </div>
-            <span class="duration">
-                <span class="amplitude-duration-minutes"></span>:<span class="amplitude-duration-seconds"></span>
-            </span>
-            </div>
-        
-            <div id="control-container">
-            <div id="repeat-container">
-                <div class="amplitude-repeat" id="repeat"></div>
-                <div class="amplitude-shuffle amplitude-shuffle-off" id="shuffle"></div>
-            </div>
-        
-            <div id="central-control-container">
-                <div id="central-controls">
-                    <div class="amplitude-prev" id="previous"></div>
-                    <div class="amplitude-play-pause" id="play-pause"></div>
-                    <div class="amplitude-next" id="next"></div>
+                <div id="time-container">
+                    <span class="current-time">
+                        <span class="amplitude-current-minutes" ></span>:<span class="amplitude-current-seconds"></span>
+                    </span>
+                    <div id="progress-container">
+                        <input type="range" class="amplitude-song-slider"/>
+                        <progress id="song-played-progress" class="amplitude-song-played-progress"></progress>
+                        <progress id="song-buffered-progress" class="amplitude-buffered-progress" value="0"></progress>
+                    </div>
+                    <span class="duration">
+                        <span class="amplitude-duration-minutes"></span>:<span class="amplitude-duration-seconds"></span>
+                    </span>
                 </div>
-            </div>
-        
-            <div id="volume-container">
-                <div class="volume-controls">
-                    <div class="amplitude-mute amplitude-not-muted"></div>
-                    <input type="range" class="amplitude-volume-slider"/>
-                    <div class="ms-range-fix"></div>
+
+                <div id="control-container">
+                    <div id="repeat-container">
+                        <div class="amplitude-repeat" id="repeat"></div>
+                        <div class="amplitude-shuffle amplitude-shuffle-off" id="shuffle"></div>
+                    </div>
+
+                    <div id="central-control-container">
+                        <div id="central-controls">
+                            <div class="amplitude-prev" id="previous"></div>
+                            <div class="amplitude-play-pause" id="play-pause"></div>
+                            <div class="amplitude-next" id="next"></div>
+                        </div>
+                    </div>
+
+                    <div id="volume-container">
+                        <div class="volume-controls">
+                            <div class="amplitude-mute amplitude-not-muted"></div>
+                            <input type="range" class="amplitude-volume-slider"/>
+                            <div class="ms-range-fix"></div>
+                        </div>
+                        <div class="amplitude-shuffle amplitude-shuffle-off" id="shuffle-right"></div>
+                    </div>
                 </div>
-                <div class="amplitude-shuffle amplitude-shuffle-off" id="shuffle-right"></div>
-            </div>
-            </div>
-            
-            <div id="meta-container">
-                <span data-amplitude-song-info="name" class="song-name"></span>
-            
-                <div class="song-artist-album">
-                    <span data-amplitude-song-info="artist"></span>
-                    <span data-amplitude-song-info="album"></span>
+
+                <div id="meta-container">
+                    <span data-amplitude-song-info="name" class="song-name"></span>
+
+                    <div class="song-artist-album">
+                        <span data-amplitude-song-info="artist"></span>
+                        <span data-amplitude-song-info="album"></span>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
         `); 
@@ -251,20 +252,7 @@ async function searchGIFs() {
         }).then(async () => {
              
             Amplitude.init({
-                "songs": [
-                    {
-                        "name": "Le bal masqué",
-                        "artist": "Opium du peuple",
-                        "album": "La révolte des opiumettes",
-                        "url": "./song/le-bal-masque.mp3",
-                    },
-                    {
-                        "name": "Poupée de cire, poupée de son",
-                        "artist": "Opium du peuple",
-                        "album": "La révolte des opiumettes",
-                        "url": "./song/poupee-de-cire.mp3",
-                    }
-                ],
+                "songs": arrayMusic,
                 callbacks: {
                     //pour démarrer la lecture à cuaque fois que l'on passe au morceau suivant ou préc
                     song_change: function () {
@@ -272,9 +260,10 @@ async function searchGIFs() {
                     }
                 }
             });
-            
 
         });
+        checkMusic();
+
     } catch (e) {
         // TODO: Set error screen
     } finally {
@@ -306,3 +295,62 @@ window.addEventListener('DOMContentLoaded', async function () {
     searchButton.addEventListener('click', searchGIFs);
     cancelButton.addEventListener('click', cancelSearch);
 });
+
+
+
+function checkMusic() {
+	/*
+		When the bandcamp link is pressed, stop all propagation so AmplitudeJS doesn't
+		play the song.
+	*/
+	let bandcampLinks = document.getElementsByClassName('bandcamp-link');
+
+	for( var i = 0; i < bandcampLinks.length; i++ ){
+		bandcampLinks[i].addEventListener('click', function(e){
+			e.stopPropagation();
+		});
+	}
+
+
+	let songElements = document.getElementsByClassName('song');
+
+	for( var i = 0; i < songElements.length; i++ ){
+		/*
+			Ensure that on mouseover, CSS styles don't get messed up for active songs.
+		*/
+		songElements[i].addEventListener('mouseover', function(){
+			this.style.backgroundColor = '#00A0FF';
+
+			this.querySelectorAll('.song-meta-data .song-title')[0].style.color = '#FFFFFF';
+			this.querySelectorAll('.song-meta-data .song-artist')[0].style.color = '#FFFFFF';
+
+			if( !this.classList.contains('amplitude-active-song-container') ){
+				this.querySelectorAll('.play-button-container')[0].style.display = 'block';
+			}
+
+			this.querySelectorAll('img.bandcamp-grey')[0].style.display = 'none';
+			this.querySelectorAll('img.bandcamp-white')[0].style.display = 'block';
+			this.querySelectorAll('.song-duration')[0].style.color = '#FFFFFF';
+		});
+
+		/*
+			Ensure that on mouseout, CSS styles don't get messed up for active songs.
+		*/
+		songElements[i].addEventListener('mouseout', function(){
+			this.style.backgroundColor = '#FFFFFF';
+			this.querySelectorAll('.song-meta-data .song-title')[0].style.color = '#272726';
+			this.querySelectorAll('.song-meta-data .song-artist')[0].style.color = '#607D8B';
+			this.querySelectorAll('.play-button-container')[0].style.display = 'none';
+			this.querySelectorAll('img.bandcamp-grey')[0].style.display = 'block';
+			this.querySelectorAll('img.bandcamp-white')[0].style.display = 'none';
+			this.querySelectorAll('.song-duration')[0].style.color = '#607D8B';
+		});
+
+		/*
+			Show and hide the play button container on the song when the song is clicked.
+		*/
+		songElements[i].addEventListener('click', function(){
+			this.querySelectorAll('.play-button-container')[0].style.display = 'none';
+		});
+	}
+}
